29924bb65a9dbc0b20c486a72efcfdef560a872b
GashparFantomu <128855619+GashparFantomu@users.noreply.github.com>
2025-03-25 08:56:57 +0200

    modificaaaaaaaari! HUAAAAAAAAA

--- src/Person.java
+++ src/Person.java
@@ -6,4 +6,18 @@ public class Person {
     private int age;
     private String prenume;
 
+    public Person(String prenume, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.prenume = prenume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }//getter, setter,
+
 }
