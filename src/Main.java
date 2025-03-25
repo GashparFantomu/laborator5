82af3b977c4adb3321823631d04598126b5af641
GashparFantomu <128855619+GashparFantomu@users.noreply.github.com>
2025-03-25 09:22:06 +0200

    modificaaaaaaaari!

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         //readingFile(); List<Persoana>list = read(fisierul.txt) si dupa dai extract method
-        List<String> names = null;
+        List<String> names = new ArrayList<>(); //evaluate expresion, sa folosim debuggerul, gandire algoritmica debugg la o functie recursiva
         try {
             File fileNames = new File("namesAndAge.txt");
             Scanner readNames = new Scanner(fileNames);
-
-            names = new ArrayList<>();
 
             while (readNames.hasNextLine()) {
                 names.add(readNames.nextLine());
@@ -24,9 +21,19 @@ public class Main {
         }
         printNames(names);
     }
+
     public static void printNames(List<String> names){
         for(String name : names){
             System.out.println(name);
+        }
+        try{
+            FileReader reader = new FileReader("namesAndAge.txt");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            List<String> namesList = new ArrayList<>();
+            System.out.println(namesList);
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
         }
     }
     /*private static void readingFile() {
